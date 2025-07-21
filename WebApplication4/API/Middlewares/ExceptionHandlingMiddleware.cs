2223d744ff316e652870974aa8845eeeb1f62089
Vinicius Eleuterio da Costa <Eleuteriodacostavinicius@gmail.com>
2025-07-21 14:26:12 -0300

    Changing error middlewere to meet the rfc 7807 pattern

--- WebApplication4/API/Middlewares/ExceptionHandlingMiddleware.cs
+++ WebApplication4/API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace NotesApp.API.Middlewares
 {
@@ -18,30 +20,53 @@ namespace NotesApp.API.Middlewares
         {
             try
             {
-                await _next(context); // segue para o próximo middleware
+                await _next(context);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Erro não tratado");
 
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = ex switch
-                {
-                    ArgumentException => StatusCodes.Status400BadRequest,
-                    KeyNotFoundException => StatusCodes.Status404NotFound,
-                    DbUpdateException => StatusCodes.Status500InternalServerError,
-                    ValidationException => StatusCodes.Status400BadRequest,
-                    _ => StatusCodes.Status500InternalServerError
-                };
+                var problemDetails = CreateProblemDetails(context, ex);
 
-                var response = new
-                {
-                    error = ex.Message,
-                    statusCode = context.Response.StatusCode
-                };
+                context.Response.ContentType = "application/problem+json";
+                context.Response.StatusCode = problemDetails.Status ?? (int)HttpStatusCode.InternalServerError;
 
-                await context.Response.WriteAsJsonAsync(response);
+                await context.Response.WriteAsJsonAsync(problemDetails);
             }
+        }
+
+        private ProblemDetails CreateProblemDetails(HttpContext context, Exception ex)
+        {
+            var statusCode = ex switch
+            {
+                ArgumentException => StatusCodes.Status400BadRequest,
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                DbUpdateException => StatusCodes.Status500InternalServerError,
+                ValidationException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return new ProblemDetails
+            {
+                Type = statusCode switch
+                {
+                    400 => "https://httpstatuses.com/400",
+                    404 => "https://httpstatuses.com/404",
+                    500 => "https://httpstatuses.com/500",
+                    _ => "https://httpstatuses.com/500"
+                },
+                Title = statusCode switch
+                {
+                    400 => "Erro de validação",
+                    404 => "Recurso não encontrado",
+                    500 => "Erro interno do servidor",
+                    _ => "Erro desconhecido"
+                },
+                Status = statusCode,
+                Detail = ex.Message,          // Detalhe do erro
+                Instance = context.Request.Path // Caminho da request
+            };
         }
     }
 }
+
