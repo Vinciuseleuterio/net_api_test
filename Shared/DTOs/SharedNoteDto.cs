957757a3f759df69006d7abbbeb3f3d11896e8ab
Vinicius Eleuterio da Costa <Eleuteriodacostavinicius@gmail.com>
2025-08-04 10:33:54 -0300

    Removing unecessary folders and classes

--- Shared/DTOs/SharedNoteDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication4.Dto
-{
-    public class SharedNoteDto
-    {
-        public int Id { get; set; }
-        public string? Title { get; init; }
-        public string? Text { get; init; }
-    }
-}
--- UserManagment/DTOs/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication4.Dto
-{
-    public class UserDto
-    {
-        public string? Name { get; init; }
-        public string? AboutMe { get; init; }
-        public string? Email { get; set; }
-    }
-}
